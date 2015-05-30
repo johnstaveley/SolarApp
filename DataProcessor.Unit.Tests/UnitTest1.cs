b00f6e6b368acd03ba6d03a5873c67929f86e68a
johnstaveley <Not telling you>
2015-05-30 07:50:44 +0100

    basic serialization test

--- DataProcessor.Unit.Tests/UnitTest1.cs
+++ DataProcessor.Unit.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NUnit.Framework;
+using Model;
 
 namespace DataProcessor.Unit.Tests
 {
@@ -7,15 +8,46 @@ namespace DataProcessor.Unit.Tests
     public class UnitTest1
     {
         [Test]
-        public void DummyTest()
+        [TestCase(53,1006,53001,123456)]
+        public void Given_SomeData_When_ISerializeADataPoint_Then_TheDataPointIsCreatedWithTheCorrectValues(int pac, int dayEnergy, int yearEnergy, int totalEnergy)
         {
-            Assert.IsTrue(true);
-        }
-
-        [Test]
-        public void DummyTest2()
-        {
-            Assert.IsTrue(true);
+            DataPoint dataPoint = new DataPoint();
+            dataPoint._id = Guid.NewGuid().ToString();
+            dataPoint.Body = new Body(){
+                PAC = new FroniusEnergyReading() {
+                    Unit = "W",
+                    Values = new FroniusEnergyReadingItem() {
+                        Value = pac
+                    }},
+                DAY_ENERGY = new FroniusEnergyReading() {
+                    Unit = "Wh",
+                    Values = new FroniusEnergyReadingItem() {
+                        Value = dayEnergy
+                    }},                    
+                    YEAR_ENERGY = new FroniusEnergyReading() {
+                    Unit = "Wh",
+                    Values = new FroniusEnergyReadingItem() {
+                        Value = yearEnergy
+                    }},
+                TOTAL_ENERGY = new FroniusEnergyReading() {
+                    Unit = "Wh",
+                    Values = new FroniusEnergyReadingItem() {
+                        Value = totalEnergy
+                    }}};
+            dataPoint.Head = new Head() {
+                Timestamp = DateTime.Now,
+                RequestArguments = new RequestArguments() {
+                    Query = "Inverter",
+                    Scope = "System"
+                },
+                Status = new Status() {
+                    Code = 0,
+                    Reason = "",
+                    UserMessage = ""
+                }
+            };
+            Assert.True(true);
+            
         }
     }
 }
