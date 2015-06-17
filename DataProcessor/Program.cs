1f75d7fbad5821c1f7dc84c93404b770eb9fddba
johnstaveley <john@staveley.org>
2015-06-17 14:08:26 +0100

    Put in simple injector (2)

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -4,22 +4,37 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleInjector;
+using DataProcessor.Utility;
+using Persistence;
 
 namespace DataProcessor
 {
     static class Program
     {
+		private static Container container;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         static void Main()
         {
             ServiceBase[] ServicesToRun;
+			Bootstrap();
             ServicesToRun = new ServiceBase[] 
             { 
                 new Service1() 
             };
             ServiceBase.Run(ServicesToRun);
         }
+
+		private static void Bootstrap()
+		{
+			container = new Container();
+			container.RegisterSingle<IConfiguration, Configuration>();
+			container.RegisterSingle<IFileSystem, FileSystem>();
+			container.RegisterSingle<ISolarAppContext, SolarAppContext>();
+			container.Verify();
+		}
     }
 }
