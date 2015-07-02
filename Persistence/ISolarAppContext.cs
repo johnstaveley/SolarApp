1f06aac1239c016307b5e333be1698f49f02a172
johnstaveley <john@staveley.org>
2015-07-02 15:02:35 +0100

    stub for weather forecast

--- Persistence/ISolarAppContext.cs
+++ Persistence/ISolarAppContext.cs
@@ -13,13 +13,16 @@ namespace SolarApp.Persistence
 		FailedData FindFailedDataById(string id);
 		DataPoint FindDataPointById(string id);
 		Setting FindSettingById(string id);
+		WeatherForecast FindWeatherForecastById(string id);
 		DateTime? GetLatestEnergyReading();
 		double? GetAverageOutputForHour(int hour);
 		void InsertDataPoint(DataPoint dataPoint);
 		void InsertFailedData(FailedData failedData);
 		void InsertSetting(Setting setting);
+		void InsertWeatherForecast(WeatherForecast weatherForecast);
 		void SeedDatabase();
 		MongoDB.Driver.MongoCollection<Setting> Settings { get; }
 		void UpdateSetting(Setting setting);
+		MongoDB.Driver.MongoCollection<WeatherForecast> WeatherForecast { get; }
 	}
 }
