f775c3fd5fd57c262e969c968f40c4408a6e6ad7
johnstaveley <john@staveley.org>
2015-07-20 07:27:19 +0100

    fixed intermittently failing unit tests

--- DataProcessor.Unit.Tests/WeatherProcessor.cs
+++ DataProcessor.Unit.Tests/WeatherProcessor.cs
@@ -11,23 +11,46 @@ namespace SolarApp.DataProcessor.Unit.Tests
 	public class WeatherProcessorTest
 	{
 
+        IConfiguration configuration;
+        IServices services;
+        ISolarAppContext solarAppContext;
+
+        [SetUp]
+        public void Setup()
+        {
+            configuration = MockRepository.GenerateMock<IConfiguration>();
+            services = MockRepository.GenerateMock<IServices>();
+            solarAppContext = MockRepository.GenerateMock<ISolarAppContext>();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            configuration = null;
+            services = null;
+            solarAppContext = null;
+        }
+
+        public void VerifyAllExpectation()
+        {
+            configuration.VerifyAllExpectations();
+            services.VerifyAllExpectations();
+            solarAppContext.VerifyAllExpectations();
+        }
+
 		[Test]
 		public void Given_WeatherForecastNotRequested_When_Process_Then_NoForecastsDownloaded()
 		{
 			// Arrange
-			var configuration = MockRepository.GenerateMock<IConfiguration>();
-			var services = MockRepository.GenerateMock<IServices>();
-			var solarAppContext = MockRepository.GenerateMock<ISolarAppContext>();
-			solarAppContext.Expect(c => c.FindSettingById("RequestWeatherForecast")).Return(new Setting() { Id = "", Value = "0" });
+            solarAppContext.Expect(c => c.FindSettingById("RequestWeatherForecast")).Return(new Setting() { Id = "RequestWeatherForecast", Value = "0" });
 
 			// Act
 			var weatherProcessor = new WeatherProcessor(configuration, solarAppContext, services);
 			var result = weatherProcessor.GetWeatherForecast();
 
 			// Assert
-			Assert.IsNull(result, "No weather download should have taken place");
-			configuration.VerifyAllExpectations();
-			solarAppContext.VerifyAllExpectations();
+			Assert.IsNull(result, "No weather forecast download should have taken place");
+            VerifyAllExpectation();
 
 		}
 
@@ -35,24 +58,52 @@ namespace SolarApp.DataProcessor.Unit.Tests
 		public void Given_WeatherForecastRequested_When_Process_Then_ForecastDownloaded()
 		{
 			// Arrange
-			var configuration = MockRepository.GenerateMock<IConfiguration>();
-			var services = MockRepository.GenerateMock<IServices>();
-			var solarAppContext = MockRepository.GenerateMock<ISolarAppContext>();
 			services.Expect(s => s.WebRequestForJson(Arg<string>.Is.Anything)).Return("{}");
-			solarAppContext.Expect(c => c.FindSettingById("RequestWeatherForecast")).Return(new Setting() { Id = "", Value = "1" });
+            solarAppContext.Expect(c => c.FindSettingById("RequestWeatherForecast")).Return(new Setting() { Id = "RequestWeatherForecast", Value = "1" });
 
 			// Act
 			var weatherProcessor = new WeatherProcessor(configuration, solarAppContext, services);
 			var weatherForecastId = weatherProcessor.GetWeatherForecast();
 
 			// Assert
-			Assert.IsTrue(!string.IsNullOrEmpty(weatherForecastId), "Forecast should have been downloaded");
-			//TODO: Assert database changes
-			configuration.VerifyAllExpectations();
-			services.VerifyAllExpectations();
-			solarAppContext.VerifyAllExpectations();
-
+			Assert.IsFalse(string.IsNullOrEmpty(weatherForecastId), "Forecast should have been downloaded");
+            solarAppContext.AssertWasCalled(c => c.InsertWeatherForecast(Arg<WeatherForecast>.Is.Anything));
+            solarAppContext.AssertWasCalled(c => c.UpdateSetting(Arg<Setting>.Is.Anything));
+            VerifyAllExpectation();
 		}
 
+        [Test]
+        public void Given_WeatherObservationNotRequested_When_Process_Then_NoObservationDownloaded()
+        {
+            // Arrange
+            solarAppContext.Expect(c => c.FindSettingById("RequestWeatherObservation")).Return(new Setting() { Id = "RequestWeatherObservation", Value = "0" });
+
+            // Act
+            var weatherProcessor = new WeatherProcessor(configuration, solarAppContext, services);
+            var result = weatherProcessor.GetWeatherObservation();
+
+            // Assert
+            Assert.IsNull(result, "No weather observation download should have taken place");
+            VerifyAllExpectation();
+        }
+
+        [Test]
+        public void Given_WeatherObservationRequested_When_Process_Then_ObservationDownloaded()
+        {
+            // Arrange
+            services.Expect(s => s.WebRequestForJson(Arg<string>.Is.Anything)).Return("{}");
+            solarAppContext.Expect(c => c.FindSettingById("RequestWeatherObservation")).Return(new Setting() { Id = "RequestWeatherObservation", Value = "1" });
+
+            // Act
+            var weatherProcessor = new WeatherProcessor(configuration, solarAppContext, services);
+            var weatherObservationId = weatherProcessor.GetWeatherObservation();
+
+            // Assert
+            Assert.IsFalse(string.IsNullOrEmpty(weatherObservationId), "Observation should have been downloaded");
+            solarAppContext.AssertWasCalled(c => c.InsertWeatherObservation(Arg<WeatherObservation>.Is.Anything));
+            solarAppContext.AssertWasCalled(c => c.UpdateSetting(Arg<Setting>.Is.Anything));
+            VerifyAllExpectation();
+
+        }
 	}
 }
