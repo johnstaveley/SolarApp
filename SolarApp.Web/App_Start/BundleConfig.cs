e1146a656b8d10a19885c5403f7b683972d383c0
johnstaveley <johnstaveley>
2015-08-18 06:46:46 +0100

    Put in basic D3 charting

--- SolarApp.Web/App_Start/BundleConfig.cs
+++ SolarApp.Web/App_Start/BundleConfig.cs
@@ -8,17 +8,14 @@ namespace SolarApp.Web
 		// For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
-
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-						"~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/d3").Include("~/Scripts/d3/d3.js"));
+			bundles.Add(new ScriptBundle("~/bundles/energyreadings").Include("~/app/energyreadings.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
 
 			// Use the development version of Modernizr to develop with and learn from. Then, when you're
 			// ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-						"~/Scripts/modernizr-*"));
-
+			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
 			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
 					  "~/Scripts/bootstrap.js",
 					  "~/Scripts/respond.js"));
--- SolarApp.Web/ViewModel/EnergyReadingsViewModel.cs
+++ SolarApp.Web/ViewModel/EnergyReadingsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using SolarApp.Model;
 
@@ -11,12 +12,16 @@ namespace SolarApp.Web.ViewModel
 
         public List<EnergyOutput> EnergyReadings { get; set; }
 
+		public string GraphData { get; set; }
+
         public bool IsDatabaseAvailable { get; set; }
 
         public EnergyReadingsViewModel(bool isDatabaseAvailable, List<EnergyOutput> energyReadings)
         {
             IsDatabaseAvailable = isDatabaseAvailable;
 			EnergyReadings = energyReadings;
+			GraphData = string.Format("{0}", energyReadings.Select(e => e.DayEnergyInstant)
+				.Aggregate(new StringBuilder(), (sb, next) => sb.Append(",").Append(next), sb => sb.ToString().Trim(',')));
         }
     }
 }
