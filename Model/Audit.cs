3ae13a155167236d9ec2edede4436de7a3a9ce77
johnstaveley <johnstaveley>
2015-08-27 05:57:08 +0100

    Added picture to front page

---
+++ Model/Audit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarApp.Model
+{
+	public class Audit
+	{
+
+		public string Who { get; set; }
+
+		public string What { get; set; }
+
+		public DateTime When { get; set; }
+
+		public string Where { get; set; }
+
+	}
+}
